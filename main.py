2941abd6557218c90cba190940a74ce7bd2da1e7
SBHANDARAM <ramsaikrishna.b@gmail.com>
2023-07-17 14:56:33 -0700

    Create README.md

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 # Rendering Predefined JSON from npoint API
@@ -9,24 +9,35 @@ blog_posts = response.json()
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def get_homepage():
     return render_template("index.html", posts=blog_posts['next_steps'])
+
 
 @app.route('/about.html')
 def get_about():
     return render_template("about.html")
 
-@app.route('/contact')
-def get_contact():
-    return render_template("contact.html")
 
+def send_email(param, param1, param2, param3):
+    print(f"Name is {param}, email is {param1}, Phone is {param2} and Message is {param3}")
+
+
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def get_contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 @app.route('/post/<int:num>')
 def get_post(num):
     print(num)
     return render_template("blog.html", post=blog_posts['next_steps'][num])
+
 
 
 if __name__ == "__main__":
